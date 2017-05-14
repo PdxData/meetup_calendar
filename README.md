7542cdeea0e2e37db4f3473c5236b0499fde4845
Allen Grimm <TheGrimmScientist@gmail.com>
2017-05-14 14:48:48 -0700

    Adding explanation of how to create local.py settings and populate with at least your meetup_api_key.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # meetup_calendar
 aggregate meetup events into a single calendar
+
+
+
+# Setup
+
+Requirements:
+   * Python 3
+
+
+## Virtual Environment
+TODO: make virtualenv
+
+## Settings
+Create a file `local.py` within the `settings` folder.
+That will put your `local.py` next to `default.py`.
+Any variables put in `local.py` will override the settings in `default.py`.
+You'll want to at least override `meetup_api_key`.
+See the block comment at the top of `default.py` for an example and brief instructions on how to do that.
+
+
--- settings/default.py
+++ settings/default.py
@@ -1,6 +1,20 @@
+"""
+
+Create a `local.py` file next to this `default.py` file to put your custom
+settings.  An example `local.py` file is:
+
+    # Get the api key associated with your Meetup account by logging in and
+    # navigating to https://secure.meetup.com/meetup_api/key/ .
+    # Never share your actual api key with anyone.
+
+    meetup_api_key = '111111111111111111111111111'
+
+"""
 
 meetup_api_key = ''
 
+# -----------  Anything above this line will be overwritten by what's in
+# `local.py` due to the following try/except import  -------------------------
 try:
     from local import *  # noqa: F401, F403
 except ImportError:
